3e89d4c7a301a499623879b21352fb610d2f0d69
Luis Lopez-Echeto <luis@lopez-echeto.com>
2022-04-22 23:31:36 +0100

    modelo Ciudad

--- ciudades/models.py
+++ ciudades/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=100)
+
+
+    class Meta:
+        verbose_name_plural = "ciudades"
+    
+
+    def __str__(self):
+        return self.nombre

78de03d23c296ab4ac7f17ef01508a1e1412edf1
Luis Lopez-Echeto <luis@lopez-echeto.com>
2022-04-23 11:30:47 +0100

    Reconfigure app dir structure

---
+++ apps/ciudades/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Ciudad
+
+class CiudadAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+
+admin.site.register(Ciudad, CiudadAdmin)
---
+++ apps/ciudades/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import PaginaInicioView
+
+urlpatterns = [
+    path('', PaginaInicioView.as_view(), name='pagina-inicio'),
+]

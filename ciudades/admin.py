e1fe4c296a2c62f29b8206ac7db2df30d33277cc
Luis Lopez-Echeto <luis@lopez-echeto.com>
2022-04-23 11:31:04 +0100

    move ciudades app to apps dir

--- ciudades/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Ciudad
-
-class CiudadAdmin(admin.ModelAdmin):
-    list_display = ("nombre",)
-
-admin.site.register(Ciudad, CiudadAdmin)
--- ciudades/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CiudadesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ciudades'

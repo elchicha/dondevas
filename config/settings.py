9a1f86b1ba33bfaf07aa8127d0e541e8b1037d11
Luis Lopez-Echeto <luis@lopez-echeto.com>
2022-04-23 11:31:37 +0100

    correct ciudades app name in settings

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'ciudades.apps.CiudadesConfig',
+    'apps.ciudades.apps.CiudadesConfig',
 ]
 
 MIDDLEWARE = [
@@ -41,7 +41,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
